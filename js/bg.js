9f064b6758eea305c3acf0eb1820cf8c21174b33
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2020-08-29 22:52:54 +0900

    set random background image
    use Math.random(), setTimeOut()

---
+++ js/bg.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 5;
+
+function removeWait(image) {
+  setTimeout(function () {
+    image.classList.remove("wait");
+  }, 50);
+}
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `img/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  image.classList.add("wait");
+  body.prepend(image);
+  image.addEventListener("loadend", removeWait(image));
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
